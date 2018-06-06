6578df32c526e185d54ae6f896e36270c006b0f0
Hello World! のTextViewを削除。 <syyyow110@gmail.com>
2018-06-06 18:13:20 +0900

    Initial commit

---
+++ app/src/main/java/jp/aftec/myapplication/MainActivity.java
@@ -0,0 +1,47 @@
+package jp.aftec.myapplication;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_product:
+                    mTextMessage.setText(R.string.title_product);
+                    return true;
+                case R.id.navigation_cart:
+                    mTextMessage.setText(R.string.title_cart);
+                    return true;
+                case R.id.navigation_delivery_status:
+                    mTextMessage.setText(R.string.title_delivery_status);
+                    return true;
+                case R.id.navigation_mypage:
+                    mTextMessage.setText(R.string.title_mypage);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
